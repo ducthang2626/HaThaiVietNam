5975d7375c02c148f6904050d14e52f4932145e2
ducthang2626 <120168165+ducthang2626@users.noreply.github.com>
2025-05-23 13:30:17 +0700

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -81,40 +81,40 @@ function NavBar() {
       }}
     >
       <NavItem label="Tìm kiếm nhanh" icon={<Search size={24} />}>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>
           Theo thương hiệu
         </a>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>
           Theo kích cỡ
         </a>
       </NavItem>
       <NavItem label="Sơn Nội - Ngoại thất cao cấp" icon={<PaintBucket size={24} />}>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>
           Theo thương hiệu
         </a>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>
           Theo kích cỡ
         </a>
       </NavItem>
       <NavItem label="Thiết bị điện - Dây cáp điện" icon={<CircuitBoard size={24} />}>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Nike Air</a>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Adidas Neo</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Nike Air</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Adidas Neo</a>
       </NavItem>
       <NavItem label="Thiết bị Vệ sinh cao cấp" icon={<Toilet size={24} />}>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
       </NavItem>
       <NavItem label="Cửa chống cháy - Cửa thép" icon={<DoorOpen size={24} />}>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
       </NavItem>
       <NavItem label="Thiết bị nước - Vật liệu" icon={<Droplets size={24} />}>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
       </NavItem>
       <NavItem label="Danh mục khác" icon={<ChartColumnStacked size={24} />}>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
-        <a href="#" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc đen</a>
+        <a href="/product" style={{ display: "block", padding: "5px 0" }}>Ba sọc trắng</a>
       </NavItem>
     </div>
   );
